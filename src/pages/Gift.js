011594dc679e915bc2d9d24e5b3f3f736dbfb54d
Tushar Desai <tusharwdesai@gmail.com>
2025-12-11 02:15:41 +0530

    Update Gift component to display 'Gift Coupons'

--- src/pages/Gift.js
+++ src/pages/Gift.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Gift = () => {
   return (
-    <div className=' md:h-[400px] md:text-2xl flex items-center justify-center'>Gift</div>
+    <div className=' md:h-[400px] md:text-2xl flex items-center justify-center'>Gift Coupons</div>
   )
 }
 
